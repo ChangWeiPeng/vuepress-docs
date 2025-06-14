6123b04d45a2948c02914525c3d9184c7d0a9ab6
ChangWeiPeng <2965714361@qq.com>
2025-06-14 12:22:32 +0800

    Update README.md

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/29657/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"pageLayout\":\"home\"},\"readingTime\":{\"minutes\":0.01,\"words\":2},\"git\":{},\"filePathRelative\":\"README.md\",\"headers\":[],\"categoryList\":[]}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"config\":[{\"type\":\"profile\",\"name\":\"Physicscw\",\"description\":\"祝爸爸父亲节快乐！\",\"avatar\":null}],\"pageLayout\":\"home\"},\"readingTime\":{\"minutes\":0.06,\"words\":17},\"git\":{},\"filePathRelative\":\"README.md\",\"headers\":[],\"categoryList\":[]}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,9 @@
 ---
 home: true
+config:
+  -
+    type: profile
+    name: Physicscw
+    description: 祝爸爸父亲节快乐！
+    avatar: 
 ---
